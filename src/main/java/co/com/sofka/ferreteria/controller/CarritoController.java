dfc35513d8779beac0df25fc9d207256aadd9cee
federzvz <85709865+federzvz@users.noreply.github.com>
2022-04-01 19:03:12 -0300

    Endpoint para procesarFacturacion

--- src/main/java/co/com/sofka/ferreteria/controller/CarritoController.java
+++ src/main/java/co/com/sofka/ferreteria/controller/CarritoController.java
@@ -1,7 +1,9 @@
 package co.com.sofka.ferreteria.controller;
 
 import co.com.sofka.ferreteria.domain.Carrito;
+import co.com.sofka.ferreteria.domain.Factura;
 import co.com.sofka.ferreteria.service.icontroller.ICarritoService;
+import co.com.sofka.ferreteria.service.icontroller.IFacturaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +17,9 @@ import reactor.core.publisher.Mono;
 public class CarritoController {
     @Autowired
     ICarritoService iCarritoService;
+
+    @Autowired
+    IFacturaService iFacturaService;
 
     @PostMapping("/")
     @ResponseStatus(HttpStatus.CREATED)
@@ -37,5 +42,21 @@ public class CarritoController {
     @GetMapping(value = "/{id}")
     private Mono<String> getById(@PathVariable String id){
         return Mono.just(this.iCarritoService.findById(id).block().toString());
+    }
+
+    @DeleteMapping(value = "/{id}")
+    private Mono<Carrito> generarFactura(@PathVariable String id){
+        Carrito c = iCarritoService.findById(id).block();
+        Factura f = new Factura(c.getFecha(),
+                c.getCliente().getNombre(),
+                c.getNombreEmpleado(),
+                c.getProductosComprados(),
+                c.getPrecioTotalAPagar());
+        return iFacturaService.save(f).then(Mono.just(this.iCarritoService.delete(id).block()));
+    }
+
+    @DeleteMapping(value = "/rechazar/{id}")
+    private Mono<Carrito> rechazarFactura(@PathVariable String id){
+        return Mono.just(this.iCarritoService.delete(id).block());
     }
 }
